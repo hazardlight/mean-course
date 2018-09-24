58eada4effe5ee1ea6b7c0086b5b86cff5b983a2
Chris Tabis <ctabis@gmail.com>
2018-09-24 17:54:01 -0400

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressSpinnerModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {PostCreateComponent} from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Post} from '../post.model';
+import {PostsService} from '../posts.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  // posts = [
+  //   {title:'First!', content:'I was first!'},
+  //   {title:'First!', content:'I was first!'},
+  //   {title:'First!', content:'I was first!'}
+  // ];
+
+  posts: Post[] = [];
+  isLoading = false;
+
+  private postsSub: Subscription;
+  constructor(public postsService: PostsService) {
+  }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+      this.isLoading = false;
+      this.posts = posts;
+    });
+  }
+
+  onDelete(postId: string){
+    this.postsService.deletePost(postId);
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
+  }
+}
